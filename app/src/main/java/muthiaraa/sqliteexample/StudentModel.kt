b39dee63651a223b7810b9517cab95ef1d662649
muthiarifkyananda <muthiarifkyananda@gmail.com>
2024-11-18 15:45:19 +0700

    push.

---
+++ app/src/main/java/muthiaraa/sqliteexample/StudentModel.kt
@@ -0,0 +1,11 @@
+package muthiaraa.sqliteexample
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class StudentModel (
+    val nim: String,
+    val name: String,
+    val age: String
+): Parcelable
